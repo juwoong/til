632a1749f8c9c130573e87e1e1592ec636dc4dc1
Juwoong Bae <juungbae@gmail.com>
2025-01-14 01:43:33 +0900

    feat: learn and exponential part to made configuable

--- sm2/sm2.py
+++ sm2/sm2.py
@@ -15,9 +15,52 @@ class SM2Result(BaseModel):
     step: t.Optional[int] = None
     ease: t.Optional[float] = None
     interval: t.Optional[float] = None
+    leech: int = 0
 
+
+DAY_IN_MINUTES = 1440
 
 class SM2:
+    hard_interval_multiplier: float
+    _min_days: int
+    _max_days: int
+    learn_intervals: t.List[int]
+    relearn_intervals: t.List[int]
+    default_ease: float
+    graduating_interval: int
+    easy_interval: int
+    easy_bonus: float
+
+    @property
+    def min_interval(self):
+        return self._min_days * DAY_IN_MINUTES
+
+    @property
+    def max_interval(self):
+        return self._max_days * DAY_IN_MINUTES
+
+    def __init__(
+        self,
+        hard_interval_multiplier: float = 1.2,
+        min_days: int = 1,
+        max_days: int = 100 * 365,
+        learn_intervals: t.List[int] = INITIAL_INTERVALS,
+        relearn_intervals: t.List[int] = INITIAL_RELEARN_INTERVALS,
+        default_ease: float = DEFAULT_EASE,
+        graduating_interval: int = GRADUATING_INTERVAL,
+        easy_interval: int = EASY_INTERVAL,
+        easy_bonus: float = EASY_BONUS
+    ):
+        self.hard_interval_multiplier = hard_interval_multiplier
+        self._min_days = min_days
+        self._max_days = max_days
+        self.learn_intervals = learn_intervals
+        self.relearn_intervals = relearn_intervals
+        self.default_ease = default_ease
+        self.graduating_interval = graduating_interval
+        self.easy_interval = easy_interval
+        self.easy_bonus = easy_bonus
+
     @staticmethod
     def get_initial_intervals() -> t.List[str]:
         return INITIAL_INTERVALS
@@ -29,32 +72,30 @@ class SM2:
             description=description,
         )
 
-    @staticmethod
-    def _handle_learning(card: Card, choice: Choice) -> SM2Result:
+    def _handle_learning(self, card: Card, choice: Choice) -> SM2Result:
         """
         return the next step and interval for the card
         """
         step = card.step
 
         if choice == Choice.AGAIN:
-            return SM2Result(phase=Phase.LEARNING, step=0, interval=INITIAL_INTERVALS[0])
+            return SM2Result(phase=Phase.LEARNING, step=0, interval=INITIAL_INTERVALS[0], leech=card.leech + 1)
         elif choice == Choice.HARD:
-            if step == 0 and len(INITIAL_INTERVALS) == 1:
-                return SM2Result(phase=Phase.LEARNING, step=0, interval=int(INITIAL_INTERVALS[0] * 1.5))
-            elif step == 0 and len(INITIAL_INTERVALS) > 1:
-                return SM2Result(phase=Phase.LEARNING, step=0, interval=(INITIAL_INTERVALS[step] + INITIAL_INTERVALS[step+1]) / 2)
+            if step == 0 and len(self.learn_intervals) == 1:
+                return SM2Result(phase=Phase.LEARNING, step=0, interval=int(self.learn_intervals[0] * 1.5))
+            elif step == 0 and len(self.learn_intervals) > 1:
+                return SM2Result(phase=Phase.LEARNING, step=0, interval=(self.learn_intervals[step] + self.learn_intervals[step+1]) / 2)
 
-            return SM2Result(phase=Phase.LEARNING, step=step, interval=INITIAL_INTERVALS[step])
+            return SM2Result(phase=Phase.LEARNING, step=step, interval=self.learn_intervals[step])
         elif choice == Choice.GOOD:
-            if step + 1 == len(INITIAL_INTERVALS):
-                return SM2Result(phase=Phase.EXPONENTIAL, step=None, ease=DEFAULT_EASE, interval=GRADUATING_INTERVAL)
+            if step + 1 == len(self.learn_intervals):
+                return SM2Result(phase=Phase.EXPONENTIAL, step=None, ease=self.default_ease, interval=self.graduating_interval)
 
-            return SM2Result(phase=Phase.LEARNING, step=step + 1, interval=INITIAL_INTERVALS[step+1])
+            return SM2Result(phase=Phase.LEARNING, step=step + 1, interval=self.learn_intervals[step+1])
         elif choice == Choice.EASY:
-            return SM2Result(phase=Phase.EXPONENTIAL, step=None, interval=EASY_INTERVAL)
+            return SM2Result(phase=Phase.EXPONENTIAL, step=None, interval=self.easy_interval)
 
-    @staticmethod
-    def _handle_exponential(card: Card, choice: Choice) -> SM2Result:
+    def _handle_exponential(self, card: Card, choice: Choice) -> SM2Result:
         ease = card.ease
         interval = card.interval
         rand_interval = random.randrange(0, 1441)
@@ -63,16 +104,16 @@ class SM2:
             ease = max(1.3, card.ease * (1 - 0.2))
 
             # TODO: add fuzz and argument -- new_interval parameter
-            interval = max(interval * LAPSE_INTERVAL_MULTIPLIER, 1440)  # At least 1 day
+            interval = max(interval * LAPSE_INTERVAL_MULTIPLIER, self.min_interval)
 
-            if len(INITIAL_RELEARN_INTERVALS) > 0:
-                return SM2Result(phase=Phase.RELEARN, ease=ease, interval=INITIAL_RELEARN_INTERVALS[0], step=0)
+            if len(self.relearn_intervals) > 0:
+                return SM2Result(phase=Phase.RELEARN, ease=ease, interval=self.relearn_intervals[0], step=0)
 
-            return SM2Result(phase=Phase.EXPONENTIAL, ease=ease, interval=interval)
+            return SM2Result(phase=Phase.EXPONENTIAL, ease=ease, interval=interval, leech=card.leech + 1)
         elif choice == Choice.HARD:
             ease = max(1.3, card.ease * (1 - 0.15))
             interval = min(
-                interval * 1.2,  # TODO: convert 1.2 to a variable -> hard_interval_multiplier
+                interval * self.hard_interval_multiplier,
                 1440 * 36500 # 100 years
             )
             # TODO: days_overdue / 4
@@ -89,7 +130,7 @@ class SM2:
         elif choice == Choice.EASY:
             ease = ease * 1.15
             interval = min(
-                (interval + rand_interval) * ease * EASY_BONUS,
+                (interval + rand_interval) * ease * self.easy_bonus,
                 1440 * 36500 # 100 years
             )
 
@@ -104,7 +145,7 @@ class SM2:
             return SM2Result(phase=Phase.EXPONENTIAL, step=None, interval=interval, ease=card.ease)
 
         if choice == Choice.AGAIN:
-            return SM2Result(phase=Phase.RELEARN, step=0, interval=INITIAL_RELEARN_INTERVALS[0])
+            return SM2Result(phase=Phase.RELEARN, step=0, interval=INITIAL_RELEARN_INTERVALS[0], leech=card.leech + 1)
         elif choice == Choice.HARD:
             if step == 0 and len(INITIAL_RELEARN_INTERVALS) == 1:
                 return SM2Result(phase=Phase.RELEARN, step=0, interval=int(INITIAL_RELEARN_INTERVALS[0] * 1.5))
