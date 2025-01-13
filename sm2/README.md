ff9cfc2f204e9c62965b59c4773513ed40cb77f1
Juwoong Bae <juungbae@gmail.com>
2025-01-14 00:57:04 +0900

    feat: sm2 anki prototype

---
+++ sm2/README.md
@@ -0,0 +1,6 @@
+# SM2 using at ANKI 
+
+## References
+
+- [SM2 Algorithm](https://www.supermemo.com/en/archives1990-2015/english/ol/sm2)
+- [Implemented at Python](https://github.com/open-spaced-repetition/anki-sm-2/blob/main/src/anki_sm_2/anki_sm_2.py)
--- sm2/sm2.py
+++ sm2/sm2.py
@@ -4,7 +4,8 @@ import typing as t
 
 from pydantic import BaseModel
 
-from const import EASY_BONUS, FIRST_EXPONENTIAL_INTERVAL, INITIAL_INTERVALS, INITIAL_RELEARN_INTERVALS, LAPSE_INTERVAL_MULTIPLIER
+from const import EASY_BONUS, GRADUATING_INTERVAL, INITIAL_INTERVALS, INITIAL_RELEARN_INTERVALS, \
+    LAPSE_INTERVAL_MULTIPLIER, EASY_INTERVAL, DEFAULT_EASE
 from dto import Choice, Phase, Card
 from libs.interval_calculator import minute_to_interval
 
@@ -13,7 +14,7 @@ class SM2Result(BaseModel):
     phase: Phase
     step: t.Optional[int] = None
     ease: t.Optional[float] = None
-    interval: t.Optional[int] = None
+    interval: t.Optional[float] = None
 
 
 class SM2:
@@ -38,21 +39,19 @@ class SM2:
         if choice == Choice.AGAIN:
             return SM2Result(phase=Phase.LEARNING, step=0, interval=INITIAL_INTERVALS[0])
         elif choice == Choice.HARD:
-            if step == 0:
-                return SM2Result(phase=Phase.LEARNING, step=0, interval=6)
-            if step == 1:
-                return SM2Result(phase=Phase.LEARNING, step=0, interval=INITIAL_INTERVALS[step])
-            if step == 3:
-                return SM2Result(phase=Phase.EXPONENTIAL, step=None, interval=INITIAL_INTERVALS[1])
-            return SM2Result(phase=Phase.LEARNING, step=step, interval=INITIAL_INTERVALS[step+1])
+            if step == 0 and len(INITIAL_INTERVALS) == 1:
+                return SM2Result(phase=Phase.LEARNING, step=0, interval=int(INITIAL_INTERVALS[0] * 1.5))
+            elif step == 0 and len(INITIAL_INTERVALS) > 1:
+                return SM2Result(phase=Phase.LEARNING, step=0, interval=(INITIAL_INTERVALS[step] + INITIAL_INTERVALS[step+1]) / 2)
+
+            return SM2Result(phase=Phase.LEARNING, step=step, interval=INITIAL_INTERVALS[step])
         elif choice == Choice.GOOD:
-            if step == 0:
-                return SM2Result(phase=Phase.LEARNING, step=1, interval=INITIAL_INTERVALS[1])
-            if step == 3:
-                return SM2Result(phase=Phase.EXPONENTIAL, step=None, interval=FIRST_EXPONENTIAL_INTERVAL)
+            if step + 1 == len(INITIAL_INTERVALS):
+                return SM2Result(phase=Phase.EXPONENTIAL, step=None, ease=DEFAULT_EASE, interval=GRADUATING_INTERVAL)
+
             return SM2Result(phase=Phase.LEARNING, step=step + 1, interval=INITIAL_INTERVALS[step+1])
         elif choice == Choice.EASY:
-            return SM2Result(phase=Phase.EXPONENTIAL, step=None, interval=FIRST_EXPONENTIAL_INTERVAL)
+            return SM2Result(phase=Phase.EXPONENTIAL, step=None, interval=EASY_INTERVAL)
 
     @staticmethod
     def _handle_exponential(card: Card, choice: Choice) -> SM2Result:
@@ -61,26 +60,70 @@ class SM2:
         rand_interval = random.randrange(0, 1441)
 
         if choice == Choice.AGAIN:
-            return SM2Result(phase=Phase.RELEARN, ease=ease - 0.2, interval=int(interval * LAPSE_INTERVAL_MULTIPLIER), step=0)
+            ease = max(1.3, card.ease * (1 - 0.2))
+
+            # TODO: add fuzz and argument -- new_interval parameter
+            interval = max(interval * LAPSE_INTERVAL_MULTIPLIER, 1440)  # At least 1 day
+
+            if len(INITIAL_RELEARN_INTERVALS) > 0:
+                return SM2Result(phase=Phase.RELEARN, ease=ease, interval=INITIAL_RELEARN_INTERVALS[0], step=0)
+
+            return SM2Result(phase=Phase.EXPONENTIAL, ease=ease, interval=interval)
         elif choice == Choice.HARD:
-            return SM2Result(phase=Phase.EXPONENTIAL, ease=ease - 0.15, interval=int(interval * 1.2))
+            ease = max(1.3, card.ease * (1 - 0.15))
+            interval = min(
+                interval * 1.2,  # TODO: convert 1.2 to a variable -> hard_interval_multiplier
+                1440 * 36500 # 100 years
+            )
+            # TODO: days_overdue / 4
+            # TODO: add fuzz to interval
+            return SM2Result(phase=Phase.EXPONENTIAL, ease=ease, interval=interval)
         elif choice == Choice.GOOD:
-            return SM2Result(phase=Phase.EXPONENTIAL, ease=ease, interval=int((interval + rand_interval) * ease))
+            interval = min(
+                (interval + rand_interval) * ease,
+                1440 * 36500 # 100 years
+            )
+
+            # TODO: fix random to fuzz interval
+            return SM2Result(phase=Phase.EXPONENTIAL, ease=ease, interval=interval)
         elif choice == Choice.EASY:
-            return SM2Result(phase=Phase.EXPONENTIAL, ease=ease + 0.15, interval=int((interval + rand_interval) * (ease * EASY_BONUS)))
+            ease = ease * 1.15
+            interval = min(
+                (interval + rand_interval) * ease * EASY_BONUS,
+                1440 * 36500 # 100 years
+            )
+
+            return SM2Result(phase=Phase.EXPONENTIAL, ease=ease, interval=interval)
 
     @staticmethod
     def _handle_relearn(card: Card, choice: Choice) -> SM2Result:
         step = card.step
 
+        if len(INITIAL_RELEARN_INTERVALS) == 0 or card.step >= len(INITIAL_RELEARN_INTERVALS):
+            interval = card.interval * card.ease
+            return SM2Result(phase=Phase.EXPONENTIAL, step=None, interval=interval, ease=card.ease)
+
         if choice == Choice.AGAIN:
             return SM2Result(phase=Phase.RELEARN, step=0, interval=INITIAL_RELEARN_INTERVALS[0])
         elif choice == Choice.HARD:
-            return SM2Result(phase=Phase.RELEARN, step=step, interval=INITIAL_RELEARN_INTERVALS[step+1] / 2)
+            if step == 0 and len(INITIAL_RELEARN_INTERVALS) == 1:
+                return SM2Result(phase=Phase.RELEARN, step=0, interval=int(INITIAL_RELEARN_INTERVALS[0] * 1.5))
+            elif step == 0 and len(INITIAL_RELEARN_INTERVALS) > 1:
+                return SM2Result(phase=Phase.RELEARN, step=0, interval=(INITIAL_RELEARN_INTERVALS[step] + INITIAL_RELEARN_INTERVALS[step+1]) / 2)
+
+            return SM2Result(phase=Phase.RELEARN, step=0, interval=INITIAL_RELEARN_INTERVALS[step])
         elif choice == Choice.GOOD:
+            if step + 1 == len(INITIAL_RELEARN_INTERVALS):
+                return SM2Result(phase=Phase.EXPONENTIAL, step=None, interval=card.interval * card.ease)
+
             return SM2Result(phase=Phase.RELEARN, step=step + 1, interval=INITIAL_RELEARN_INTERVALS[step+1])
         elif choice == Choice.EASY:
-            return SM2Result(phase=Phase.EXPONENTIAL, step=None, interval=INITIAL_RELEARN_INTERVALS[-1] + FIRST_EXPONENTIAL_INTERVAL)
+            interval = min(
+                card.interval * card.ease * EASY_BONUS,
+                1440 * 36500 # 100 years
+            )
+
+            return SM2Result(phase=Phase.EXPONENTIAL, step=None, interval=interval)
 
     @staticmethod
     def get_next_card(card, choice) -> SM2Result:
